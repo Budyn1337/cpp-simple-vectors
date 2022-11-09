c76dd5bd5b4ddfc74b3f890db0c4af18f80d6b09
Michał Medyński <58748363+Budyn1337@users.noreply.github.com>
2022-11-09 14:18:36 +0100

    Update color.hpp
    new constructors

--- color.hpp
+++ color.hpp
@@ -8,6 +8,9 @@
 #include "vector4.hpp"
 
 #include <algorithm>
+#include <type_traits>
+
+using HexColor = unsigned long;
 
 struct Color
 {
@@ -17,24 +20,52 @@ struct Color
 	float a;
 
 	Color();
+
+	template<typename T> Color(T r, T g, T b, T a); //constructor for integral types
+
 	Color(float r, float g, float b, float a = 1.0f);
-	Color(unsigned int r, unsigned int g, unsigned int b, unsigned int a = 255u);
 
-	Color(unsigned long hexColor);
+	Color(HexColor hex);
+	Color(HexColor hex, float a);
 
-	Color(const Color& copy);
+	Color(const Color&) = default;
 
-	void operator=(const Color& color);
-	void operator=(unsigned long hex);
+	void operator=(const Color&);
+	void operator=(HexColor hex);
 
 	bool operator==(const Color& color) const;
 	bool operator!=(const Color& color) const;
 
 	operator Vector4<float>() const;
 
-	unsigned long GetHexColor()  const;
+	HexColor GetHexColor() const;
 
-	static Color HexToRgb(unsigned long hex);
+	static Color HexToRgb(HexColor hex);
 };
 
+template<typename T>
+inline Color::Color(T r, T g, T b, T a)
+{
+	static_assert(
+		std::is_floating_point_v<T> || std::is_integral_v<T>, 
+		"Color value must be floating point or integral"
+	);
+
+	if (std::is_floating_point_v<T>)
+	{
+		this->r = static_cast<float>(r);
+		this->g = static_cast<float>(g);
+		this->b = static_cast<float>(b);
+		this->a = static_cast<float>(a);
+	}
+	else
+	{
+		this->r = static_cast<float>(std::min<T>(std::max<T>(r, 0), 255) / 255.0f);
+		this->g = static_cast<float>(std::min<T>(std::max<T>(g, 0), 255) / 255.0f);
+		this->b = static_cast<float>(std::min<T>(std::max<T>(b, 0), 255) / 255.0f);
+		this->a = static_cast<float>(std::min<T>(std::max<T>(a, 0), 255) / 255.0f);
+	}
+}
+
 #endif
+
